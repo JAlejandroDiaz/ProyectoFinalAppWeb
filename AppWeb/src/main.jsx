af7de35d98d14e6a50e917f93371889bd3e780a6
ander0898 <128049886+ander0898@users.noreply.github.com>
2023-06-10 15:15:20 -0500

    111

--- AppWeb/src/main.jsx
+++ AppWeb/src/main.jsx
@@ -4,10 +4,11 @@ import './index.css'
 import ConsumoDeApi from './components/ConsumoDeApi.jsx'
 import { Registar } from './components/registar'
 import { ConsumoAmazon } from './components/ConsumoAmazon'
+import { PeliculasApi } from './pages/PeliculasApi'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    < ConsumoAmazon />
+    < PeliculasApi />
   </React.StrictMode>,
 )
