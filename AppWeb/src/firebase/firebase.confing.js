651cbc25ac7fa315ad8d2404de287c41e6d10227
ander0898 <128049886+ander0898@users.noreply.github.com>
2023-06-19 18:29:16 -0500

    yaima4.com

--- AppWeb/src/firebase/firebase.confing.js
+++ AppWeb/src/firebase/firebase.confing.js
@@ -2,6 +2,9 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import {getAuth} from 'firebase/auth'
+import { getFirestore } from "firebase/firestore";
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore'
 const firebaseConfig = {
   apiKey: "AIzaSyCIV499woR85yWg5A2gyBzjYdVjRzoidZw",
   authDomain: "peliculas-92484.firebaseapp.com",
@@ -11,8 +14,13 @@ const firebaseConfig = {
   appId: "1:152830185862:web:9283fe2c3bd962b73a1c07",
   measurementId: "G-YJ035CBV8K"
 };
+firebase.initializeApp(firebaseConfig);
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-export const auth = getAuth(app)
+ const auth = getAuth(app)
+ const firestore  = getFirestore(app);
+ const db = firebase.firestore();
+
+export {app, auth, firestore, db }
--- AppWeb/src/peliculas/pages/PagePelicula.jsx
+++ AppWeb/src/peliculas/pages/PagePelicula.jsx
@@ -1,22 +1,52 @@
 import React, { useEffect, useState } from "react";
 import { Navigate, useNavigate, useParams } from "react-router-dom";
 import { BuscarId } from "../../peliculas/helpers/BuscarId";
+//import { firebase } from "../../firebase/firebase.confing";
+import { db  } from '../../firebase/firebase.confing';
 import "./Style.css";
 export const PagePelicula = () => {
   const { movieId } = useParams();
   const [movie, setmovie] = useState("");
   const [genero, setgenero] = useState([])
+  const [ListaLinks, setListaLinks] = useState([])
   const URL_IMAGEN = "https://image.tmdb.org/t/p/original";
 
+ 
+useEffect(()=>{
+  const ensayo = async () => {
+    try {
+      const docRef = db.collection("Peliculas").doc(movieId);
+      const doc = await docRef.get();
+      if (doc.exists) {
+        const data = doc.data();
+        console.log(data.pelicula)
+         setListaLinks(Object.values(data))
+        // if (data.hasOwnProperty("pelicula") && data.pelicula.hasOwnProperty("link1") && data.pelicula.link1) {
+        //   const link1 = data.pelicula.link1;
+        //   console.log(link1);
+        // } else {
+        //   console.log("El campo link1 no está presente en el objeto pelicula del documento o su valor es undefined.");
+        // }
+      } else {
+        console.log("Este dato no existe!");
+      }
+    } catch (error) {
+      console.log("Error al obtener el documento:", error);
+    }
+  };
+  ensayo()
 
-
+},[1])
+  
+  
+  
   useEffect(() => {
     BuscarId(movieId, setmovie , setgenero);
     
 
   }, [movieId]);
   // console.log(movieId)
-  console.log(movie);
+ // console.log(movie);
 
   const containerStyle = {
     backgroundImage: `linear-gradient(to bottom, rgba(0,0,0,0.5), rgba(0,0,0,2)),url(${URL_IMAGEN}/${movie.poster_path})`,
@@ -65,10 +95,22 @@ export const PagePelicula = () => {
             <h4>Puntuacion general</h4>
             <label className="mt-3 botonCircilar">{movie.vote_average}</label>
                 
-            
+            {
+              ListaLinks.map(val=>{
+                return (<div key={val.id_pel}>
+                      <a className="btn btn-primary" onClick={()=>{
+                        window.open(val.link1, '-blank')
+                      }}>opcion #1</a>
+                      <a className="btn btn-warning" onClick={()=>{
+                        window.open(val.link2, '-blank')
+                      }}>opcion #2</a>
+                </div>)
+              })
+            }
             </div>
           </div>
         </div>
+        
       </div>
     </>
   );
