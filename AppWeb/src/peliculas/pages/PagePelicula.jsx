c842717663f62770756e30abc4e396c53fe13b88
ander0898 <128049886+ander0898@users.noreply.github.com>
2023-06-27 21:53:17 -0500

    siu

--- AppWeb/src/peliculas/pages/PagePelicula.jsx
+++ AppWeb/src/peliculas/pages/PagePelicula.jsx
@@ -2,51 +2,67 @@ import React, { useEffect, useState } from "react";
 import { Navigate, useNavigate, useParams } from "react-router-dom";
 import { BuscarId } from "../../peliculas/helpers/BuscarId";
 //import { firebase } from "../../firebase/firebase.confing";
-import { db  } from '../../firebase/firebase.confing';
+import { db } from "../../firebase/firebase.confing";
 import "./Style.css";
+import { Crearcomentario } from "./Crearcomentario";
+import { Comentarios } from "./Comentarios";
 export const PagePelicula = () => {
   const { movieId } = useParams();
   const [movie, setmovie] = useState("");
-  const [genero, setgenero] = useState([])
-  const [ListaLinks, setListaLinks] = useState([])
+  const [genero, setgenero] = useState([]);
+  const [ListaLinks, setListaLinks] = useState([]);
+  const [ListaComentarios, setListaComentarios] = useState([]);
   const URL_IMAGEN = "https://image.tmdb.org/t/p/original";
+  const [selectMenu, setselectMenu] = useState(false);
 
- 
-useEffect(()=>{
-  const ensayo = async () => {
-    try {
-      const docRef = db.collection("Peliculas").doc(movieId);
-      const doc = await docRef.get();
-      if (doc.exists) {
-        const data = doc.data();
-        console.log(data.pelicula)
-         setListaLinks(Object.values(data))
-        // if (data.hasOwnProperty("pelicula") && data.pelicula.hasOwnProperty("link1") && data.pelicula.link1) {
-        //   const link1 = data.pelicula.link1;
-        //   console.log(link1);
-        // } else {
-        //   console.log("El campo link1 no está presente en el objeto pelicula del documento o su valor es undefined.");
-        // }
-      } else {
-        console.log("Este dato no existe!");
-      }
-    } catch (error) {
-      console.log("Error al obtener el documento:", error);
-    }
-  };
-  ensayo()
-
-},[1])
-  
-  
-  
   useEffect(() => {
-    BuscarId(movieId, setmovie , setgenero);
-    
+    const ensayo = async () => {
+      try {
+        const docRef = db.collection("Peliculas").doc(movieId);
+        const doc = await docRef.get();
+        if (doc.exists) {
+          const data = doc.data();
+          //console.log(data.pelicula);
+          setListaLinks(Object.values(data));
+          // if (data.hasOwnProperty("pelicula") && data.pelicula.hasOwnProperty("link1") && data.pelicula.link1) {
+          //   const link1 = data.pelicula.link1;
+          //   console.log(link1);
+          // } else {
+          //   console.log("El campo link1 no está presente en el objeto pelicula del documento o su valor es undefined.");
+          // }
+        } else {
+          console.log("Este dato no existe!");
+        }
+      } catch (error) {
+        console.log("Error al obtener el documento:", error);
+      }
+    };
+    ensayo();
+    const comentario = async () =>{
+      try {
+        const docRef = db.collection("Comments").doc(movieId);
+        const doc = await docRef.get();
+        console.log(doc)
+        if (doc.exists) {
+          const data = doc.data();
+          console.log(data);
+          setListaComentarios(Object.values(data));
+          
+        } else {
+          console.log("Este dato no existe!");
+        }
+      } catch (error) {
+        console.log("Error al obtener el documento:", error);
+      }
+    }
+    comentario()
+  }, [1]);
 
+  useEffect(() => {
+    BuscarId(movieId, setmovie, setgenero);
   }, [movieId]);
   // console.log(movieId)
- // console.log(movie);
+  // console.log(movie);
 
   const containerStyle = {
     backgroundImage: `linear-gradient(to bottom, rgba(0,0,0,0.5), rgba(0,0,0,2)),url(${URL_IMAGEN}/${movie.poster_path})`,
@@ -64,7 +80,6 @@ useEffect(()=>{
 
   return (
     <>
-  
       <div style={containerStyle}>
         <div className="container">
           <div className="grid-container">
@@ -82,63 +97,100 @@ useEffect(()=>{
               <h4 className="mt-3">Fecha de lanzamiento:</h4>
               <h4 className="mt-1">{movie.release_date}</h4>
               <div className="categorias mt-3">
-                {
-                    genero.map((val,i)=>{
-                      return(
-                        <label className=" btn-oval"  key={i}>{val.name}</label>
-                      )
-                    })
-                }
+                {genero.map((val, i) => {
+                  return (
+                    <label className=" btn-oval" key={i}>
+                      {val.name}
+                    </label>
+                  );
+                })}
               </div>
-              
-            <h4 className="mt-5">Descripcion</h4>
-            <p>{movie.overview}</p>
-            <h4>Puntuacion general</h4>
-            <label className="mt-3 botonCircilar">{movie.vote_average}</label>
-                
-            {
-              ListaLinks.map(val=>{
-                return (<div key={val.id_pel}>
-                      <a className="btn btn-primary" onClick={()=>{
-                        window.open(val.link1, '-blank')
-                      }}>opcion #1</a>
-                      <a className="btn btn-warning" onClick={()=>{
-                        window.open(val.link2, '-blank')
-                      }}>opcion #2</a>
-                </div>)
-              })
-            }
+
+              <h4 className="mt-5">Descripcion</h4>
+              <p>{movie.overview}</p>
+              <h4>Puntuacion general</h4>
+              <label className="mt-3 botonCircilar">{movie.vote_average}</label>
+
+              {ListaLinks.map((val) => {
+                return (
+                  <div key={val.id_pel}>
+                    <a
+                      className="btn btn-primary"
+                      onClick={() => {
+                        window.open(val.link1, "-blank");
+                      }}
+                    >
+                      opcion #1
+                    </a>
+                    <a
+                      className="btn btn-warning"
+                      onClick={() => {
+                        window.open(val.link2, "-blank");
+                      }}
+                    >
+                      opcion #2
+                    </a>
+                  </div>
+                );
+              })}
             </div>
           </div>
-          
-
-
-
-
-
-
-
         </div>
-        
       </div>
 
-      <div className="container mt-3 ">
-      <div className="comment-box">
-      <hr />
-    {/* Espacio para un fururo map */}
-    
-
-
-
-    
-     <hr />
-    </div>
-
-    </div>
-
-
-
-
+      <div className="container mt-3 mb-5 ">
+        <div className="comment-box">
+          <div className="row">
+            <div className="col-6 d-flex justify-content-center ">
+              <button
+                className="btn btn-warning"
+                onClick={() => {
+                  setselectMenu(false);
+                }}
+              >
+                Comentarios
+              </button>
+            </div>
+            <div className="col-6 d-flex justify-content-center">
+              <button
+                className="btn btn-primary"
+                onClick={() => {
+                  setselectMenu(true);
+                }}
+              >
+                Aportar
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      {selectMenu === false ? (
+        <>
+          <div
+            style={{
+              width: "100%",
+              overflow: "auto",
+              maxHeight: "300px",
+              overflowY: "scroll",
+            }}
+          >
+            <div className="container ">
+              <div className="row">
+                <div className="col-12">
+                  <div className="card">
+                    
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <Comentarios />
+        </>
+      ) : (
+        <>
+          <Crearcomentario />
+        </>
+      )}
     </>
   );
 };
